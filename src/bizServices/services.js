983ad0153c9c6286da37d02f24db48a9eb88549b
Collins Olowolagba <oluwafemicollins707@gmail.com>
2024-11-07 07:29:31 +0100

    first commit

--- src/bizServices/services.js
+++ src/bizServices/services.js
@@ -3,7 +3,6 @@ import AllNav from '../navbar/navbar'
 import Footer from '../footer/footer'
 import ServiceBackground from './serviceBackground/serviceBackground'
 import ServiceBoxes from './service-boxes/servicesboxes'
-import OtherServices from './otherServices/otherServices'
 
 function BizServices() {
     return(
